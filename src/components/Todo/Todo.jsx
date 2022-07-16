f7c13652dae0a4514548d477e296c54c375c36e0
BogdanMyagkiy <myagkiybogdan@gmail.com>
2022-07-16 17:19:00 +0300

    todos

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -2,7 +2,7 @@ import { Text } from 'components';
 import { TodoWrapper, DeleteButton, EditButton } from './Todo.styled';
 import { RiDeleteBinLine, RiEdit2Line } from 'react-icons/ri';
 
-export const Todo = ({ text, elementIndex, handleDelete }) => {
+export const Todo = ({ text, elementIndex, handleDelete, handleEdit }) => {
   return (
     <TodoWrapper>
       <Text textAlign="center" marginBottom="20px">
@@ -12,6 +12,9 @@ export const Todo = ({ text, elementIndex, handleDelete }) => {
       <DeleteButton type="button" onClick={handleDelete}>
         <RiDeleteBinLine size={24} />
       </DeleteButton>
+      <EditButton type="button" onClick={handleEdit}>
+        <RiEdit2Line size={24} />
+      </EditButton>
     </TodoWrapper>
   );
 };
--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -5,6 +5,8 @@ import { Grid, GridItem, SearchForm, EditForm, Text, Todo } from 'components';
 export class Todos extends Component {
   state = {
     todos: [],
+    isEditing: false,
+    currentTodo: {},
   };
 
   componentDidMount() {
@@ -38,11 +40,60 @@ export class Todos extends Component {
     }));
   };
 
-  render() {
+  editTodo = todo => {
+    this.setState({ isEditing: true, currentTodo: { ...todo } });
+  };
+
+  canselEditTodo = () => {
+    this.setState({ isEditing: false });
+  };
+
+  handleInputEditChange = event => {
+    const { currentTodo } = this.state;
+    this.setState({
+      currentTodo: { ...currentTodo, text: event.target.value },
+    });
+  };
+
+  handleUpdateTodo = (id, updatedTodo) => {
     const { todos } = this.state;
+
+    const updatedItem = todos.map(todo => {
+      return todo.id === id ? updatedTodo : todo;
+    });
+
+    this.setState({
+      isEditing: false,
+      todos: updatedItem,
+    });
+  };
+
+  handleEditFormUpdate = e => {
+    e.preventDefault();
+
+    const { currentTodo } = this.state;
+
+    if (currentTodo.text.trim() === '') {
+      return alert('Enter some text!');
+    }
+
+    this.handleUpdateTodo(currentTodo.id, currentTodo);
+  };
+
+  render() {
+    const { todos, isEditing, currentTodo } = this.state;
     return (
       <>
-        <SearchForm onSubmit={this.handleSubmit} />
+        {isEditing ? (
+          <EditForm
+            onCansel={this.canselEditTodo}
+            currentTodo={currentTodo}
+            onChange={this.handleInputEditChange}
+            onEdit={this.handleEditFormUpdate}
+          />
+        ) : (
+          <SearchForm onSubmit={this.handleSubmit} />
+        )}
         <Grid>
           {todos.map(({ id, text }, index) => (
             <GridItem key={id}>
@@ -50,6 +101,7 @@ export class Todos extends Component {
                 text={text}
                 elementIndex={index + 1}
                 handleDelete={() => this.deleteTodo(id)}
+                handleEdit={() => this.editTodo({ id, text })}
               />
             </GridItem>
           ))}

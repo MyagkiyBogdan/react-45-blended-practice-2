4a584b4dd9fcd2c6e0eb287a92a8f02ef3709059
BogdanMyagkiy <myagkiybogdan@gmail.com>
2022-07-16 16:49:24 +0300

    todos added

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -12,6 +12,7 @@ export class SearchForm extends Component {
     e.preventDefault();
 
     this.props.onSubmit(this.state.query);
+    this.setState({ query: '' });
   };
 
   handleInputChange = e => {
--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -3,7 +3,58 @@ import { nanoid } from 'nanoid';
 import { Grid, GridItem, SearchForm, EditForm, Text, Todo } from 'components';
 
 export class Todos extends Component {
+  state = {
+    todos: [],
+  };
+
+  componentDidMount() {
+    const parsedTodos = JSON.parse(localStorage.getItem('todos')) ?? [];
+    this.setState({ todos: parsedTodos });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { todos } = this.state;
+
+    if (prevState.todos !== todos) {
+      localStorage.setItem('todos', JSON.stringify(todos));
+    }
+  }
+
+  handleSubmit = data => {
+    this.addTodo(data);
+  };
+
+  addTodo = text => {
+    const todo = {
+      id: nanoid(),
+      text,
+    };
+    this.setState(prevState => ({ todos: [...prevState.todos, todo] }));
+  };
+
+  deleteTodo = id => {
+    this.setState(prevState => ({
+      todos: prevState.todos.filter(todo => todo.id !== id),
+    }));
+  };
+
   render() {
-    return <Text>Todos</Text>;
+    const { todos } = this.state;
+    return (
+      <>
+        <SearchForm onSubmit={this.handleSubmit} />
+        <Grid>
+          {todos.map(({ id, text }, index) => (
+            <GridItem key={id}>
+              <Todo
+                text={text}
+                elementIndex={index + 1}
+                handleDelete={() => this.deleteTodo(id)}
+              />
+            </GridItem>
+          ))}
+        </Grid>
+      </>
+    );
   }
 }
